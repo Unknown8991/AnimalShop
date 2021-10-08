4a68b7b5ffe0bf7e67c6c800e93ed628dc0ba3b6
Unknown8991 <rafal.pat@onet.pl>
2021-10-08 14:21:35 +0200

    first commit

---
+++ product-data.js
@@ -0,0 +1,10 @@
+data: {
+  sitename: "Skład dla zwierzaków :: Vue.js",
+  product: {
+    id: 1001,     	
+    title: "Karma dla kota 10 kg",
+    description: "10-kilogramowe opakowanie pysznej karmy organicznej, której żaden kot się nie <em>oprze</em>!",
+    price: 6700,	
+    image: "assets/images/product-fullsize.png"
+  }	
+},
